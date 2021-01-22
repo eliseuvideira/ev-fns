5b8775dffa377b4a0615338d3a0263615e70db77
Eliseu Videira <eliseuvideira1@gmail.com>
2021-01-22 16:18:23 -0300

    :sparkles: added way to override search for tokenon request object

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,15 +1,23 @@
+import { Request } from "express";
 import { endpoint } from "@ev-fns/endpoint";
 import { HttpError } from "@ev-fns/errors";
 
 interface AuthProps {
   token: string;
+  getToken?: (req: Request) => string | undefined;
 }
 
 const TOKEN_PREFIX = "Bearer ";
 
-const auth = ({ token }: AuthProps) =>
-  endpoint(async (req, res, next) => {
-    const auth = req.headers["authorization"];
+const auth = ({ token, getToken }: AuthProps) => {
+  let getTokenDefault = (req: Request) => req.headers["authorization"];
+
+  if (getToken) {
+    getTokenDefault = getToken;
+  }
+
+  return endpoint(async (req, res, next) => {
+    const auth = getTokenDefault(req);
 
     if (!auth || !auth.startsWith(TOKEN_PREFIX)) {
       res.set("WWW-Authenticate", "Bearer");
@@ -24,5 +32,6 @@ const auth = ({ token }: AuthProps) =>
 
     next();
   });
+};
 
 export = auth;
