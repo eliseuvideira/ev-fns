1fb6a7bbcbfbccfbdce6090cc14aee69d18f8c12
Eliseu Videira <eliseuvideira1@gmail.com>
2021-01-30 14:21:08 -0300

    jwt: :bug: added parameter to specify expiration

--- packages/jwt/src/functions/encode.ts
+++ packages/jwt/src/functions/encode.ts
@@ -1,8 +1,12 @@
 import jsonwebtoken from "jsonwebtoken";
 
-export const encode = async (token: Record<string, any>, secret: string) =>
+export const encode = async (
+  token: Record<string, any>,
+  secret: string,
+  options?: jsonwebtoken.SignOptions
+) =>
   new Promise<string>((resolve, reject) =>
-    jsonwebtoken.sign(token, secret, (err, token) =>
+    jsonwebtoken.sign(token, secret, options || {}, (err, token) =>
       err ? reject(err) : resolve(token || "")
     )
   );
