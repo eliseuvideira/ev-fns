334ec59cff860785560b6050cd801967981e6676
Eliseu Videira <eliseuvideira1@gmail.com>
2021-02-05 10:49:38 -0300

    model: :wrench: adding exports

--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -1,1 +1,3 @@
 export { createModel } from "./functions/createModel";
+export { createSchema } from "./functions/createSchema";
+export { createSchemaSearch } from "./functions/createSchemaSearch";
