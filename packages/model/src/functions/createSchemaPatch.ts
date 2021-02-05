825df096bf87acd6d0064fcfe8bb6663738e0128
Eliseu Videira <eliseuvideira1@gmail.com>
2021-02-05 11:00:05 -0300

    model: :wrench: added create schema patch

---
+++ packages/model/src/functions/createSchemaPatch.ts
@@ -0,0 +1,6 @@
+import Joi from "joi";
+import { SchemaProps } from "../common/SchemaProps";
+
+export const createSchemaPatch = <T>(
+  props: SchemaProps<Partial<T>>
+): Joi.ObjectSchema => Joi.object().keys(props).min(1).required();
--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -1,3 +1,4 @@
 export { createModel } from "./functions/createModel";
 export { createSchema } from "./functions/createSchema";
 export { createSchemaSearch } from "./functions/createSchemaSearch";
+export { createSchemaPatch } from "./functions/createSchemaPatch";
