8cdc92d3dbf503c517097ebd57baa99fba5d98b6
Eliseu Videira <eliseuvideira1@gmail.com>
2021-02-08 10:11:36 -0300

    jwt: :bug: fixed missing typings

--- packages/jwt/src/@types/express/index.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Express {
-  export interface Request {
-    token: Record<string, any> | undefined;
-    _token?: {
-      raw?: string;
-      value?: Record<string, any> | undefined;
-    };
-  }
-}
--- packages/jwt/src/index.ts
+++ packages/jwt/src/index.ts
@@ -1,3 +1,15 @@
+declare global {
+  export namespace Express {
+    export interface Request {
+      token: Record<string, any> | undefined;
+      _token?: {
+        raw?: string;
+        value?: Record<string, any> | undefined;
+      };
+    }
+  }
+}
+
 export { decode } from "./functions/decode";
 export { encode } from "./functions/encode";
 export { jwt } from "./functions/jwt";
