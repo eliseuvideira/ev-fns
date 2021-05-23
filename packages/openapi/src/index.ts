306210bf6af958b362fef33a92fb1901faadcfe8
Eliseu Videira <eliseuvideira1@gmail.com>
2021-05-23 10:28:12 -0300

    :sparkles: added openapi.yaml file as route option

--- packages/openapi/src/index.ts
+++ packages/openapi/src/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { serve, setup } from "swagger-ui-express";
 import { createConfig } from "./createConfig";
+import YAML from "yaml";
 
 interface OpenAPIOptions {
   path?: string;
@@ -27,7 +28,13 @@ const openapi = ({
 
   const config = createConfig({ apiName, apiVersion });
 
+  const yaml = YAML.stringify(config);
+
   router.use(path || "/api-docs", serve, setup(config));
+
+  router.get((path || "/api-docs") + "/openapi.yml", (req, res) =>
+    res.status(200).send(yaml).end()
+  );
 
   return router;
 };
